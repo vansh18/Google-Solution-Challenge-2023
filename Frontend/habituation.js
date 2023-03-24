1e4362a63d9ebd483194d060397f07ea63c06306
Vansh <vansh.gupta850@gmail.com>
2023-03-24 12:02:01 +0400

    added habituation.js and prompt for addiction

---
+++ Frontend/habituation.js
@@ -0,0 +1,27 @@
+function CustomPrompt(){
+	this.render = function(dialog,func){
+		var winW = window.innerWidth;
+	    var winH = window.innerHeight;
+		var dialogoverlay = document.getElementById('dialogoverlay');
+	    var dialogbox = document.getElementById('dialogbox');
+		dialogoverlay.style.display = "block";
+	    dialogoverlay.style.height = winH+"px";
+		dialogbox.style.left = (winW/2) - (550 * .5)+"px";
+	    dialogbox.style.top = "100px";
+	    dialogbox.style.display = "block";
+	    document.getElementById('dialogboxbody').innerHTML = dialog;
+		document.getElementById('dialogboxbody').innerHTML += '<br><input id="prompt_value1">';
+		document.getElementById('dialogboxfoot').innerHTML = '<button onclick="Prompt.ok(\''+func+'\')">Add</button> <button onclick="Prompt.cancel()">Cancel</button>';
+	}
+	this.cancel = function(){
+		document.getElementById('dialogbox').style.display = "none";
+		document.getElementById('dialogoverlay').style.display = "none";
+	}
+	this.ok = function(func){
+		var prompt_value1 = document.getElementById('prompt_value1').value;
+		window[func](prompt_value1);
+		document.getElementById('dialogbox').style.display = "none";
+		document.getElementById('dialogoverlay').style.display = "none";
+	}
+}
+var Prompt = new CustomPrompt();
