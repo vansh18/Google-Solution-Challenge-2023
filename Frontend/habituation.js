fc990951826e4197cd3b3b8dd4d04957e7122480
Vansh <vansh.gupta850@gmail.com>
2023-03-29 19:51:58 +0400

    completed habituation

--- Frontend/habituation.js
+++ Frontend/habituation.js
@@ -1,5 +1,7 @@
-function CustomPrompt(){
-	this.render = function(dialog,func){
+function CustomPrompt()
+{
+	this.render = function(dialog,func)
+	{
 		var winW = window.innerWidth;
 	    var winH = window.innerHeight;
 		var dialogoverlay = document.getElementById('dialogoverlay');
@@ -13,16 +15,25 @@ function CustomPrompt(){
 		document.getElementById('dialogboxbody').innerHTML += '<br><input id="prompt_value1" autocomplete="off">';
 		document.getElementById('dialogboxfoot').innerHTML = '<button onclick="Prompt.ok(\''+func+'\')">Add</button> <button onclick="Prompt.cancel()">Cancel</button>';
 	}
-	this.cancel = function(){
+	this.cancel = function()
+	{
 		document.getElementById('dialogbox').style.display = "none";
 		document.getElementById('dialogoverlay').style.display = "none";
 	}
-	this.ok = function(func){
+	this.ok = function(func)
+	{
 		var prompt_value1 = document.getElementById('prompt_value1').value;
 		// window[func](prompt_value1);
-        console.log(prompt_value1);
+        console.log(prompt_value1); //  --------- ALAN YOUR PROMPT VALUE IS HERE USE THIS --------
 		document.getElementById('dialogbox').style.display = "none";
 		document.getElementById('dialogoverlay').style.display = "none";
 	}
 }
 var Prompt = new CustomPrompt();
+
+const toggleBtn = document.querySelector('.toggle_btn');
+const links = document.querySelector('.links');
+
+toggleBtn.addEventListener('click', () => {
+  links.classList.toggle('active1');
+});
